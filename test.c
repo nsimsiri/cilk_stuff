d27406f552ebcfb15bcf5fd9009691d1b849dd48
NatchaS <nsimsiri@umass.edu>
2015-06-11 00:37:51 +0700

    first commit

---
+++ test.c
@@ -0,0 +1,19 @@
+#include <cilk/cilk.h>
+#include <assert.h>
+#include <stdio.h>
+
+int fib(int n) {
+  if (n < 2)
+    return n;
+  int a = cilk_spawn fib(n-1);
+  int b = fib(n-2);
+  cilk_sync;
+  return a + b;
+}
+
+int main() {
+  int result = fib(30);
+  printf("%d\n", result);
+  assert(result == 832040);
+  return 0;
+} 
