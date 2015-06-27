de2bb0ef428f6f360d1a4f563c4b74ff7f691f12
NatchaS <nsimsiri@umass.edu>
2015-06-28 03:46:58 +0700

    ..

--- utils.cpp
+++ utils.cpp
@@ -13,13 +13,39 @@ T* newA(int n, T value){
 	return A;
 }
 
-template <class Array, class F>
-int reduce(Array *Out, Array *In, int n, F f){
+template <class E, class F>
+E* map(E* In, int n, F f){
+	E* Out = newA(n, 0);
+	cilk_for(int i = 0; i < n; i++){ Out[i]=f(In[i]);}
+	return Out;
+}
+
+template <class E, class F>
+E* reduce_w_partial_prefix(E *In, int n, F f){
+	E* Out = map(In, [](int x){return x;});
 	for(int i = 0; i < log2(n); i++){
-		cilk_for(int j = 0; j < n)
+		int d = pow(2,i+1);
+		cilk_for(int j = pow(2,i)-1; j < n; j+=d){
+			int next = j+(d/2);
+			if (next<n){ Out[next]+=Out[j];}	
+		}
 	}
+	return Out;
+}
+
+template <class E, class F>
+E reduce(E *In, int n, F f){ 
+	E* Out = reduce_w_partial_prefix(In, n, f);
+	E result = Out[n-1];
+	free(E);
+	return result;
 }
 
 int main(){
 	cout << "parallel functional interface written by Natcha Simsiri\n";
+	int A[] = {1,2,3};
+	int *B = map(A, sizeof(A)/sizeof(int), [](int x){return x;});
+	for(int i = 0 ; i < sizeof(A)/sizeof(int); i++){
+		cout << B[i] << " ";
+	}
 }
