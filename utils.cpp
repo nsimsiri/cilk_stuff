4d754ed8dc30781ab89218326577df567d6ab621
NatchaS <nsimsiri@umass.edu>
2015-07-03 08:46:58 +0700

    .

--- utils.cpp
+++ utils.cpp
@@ -38,7 +38,7 @@ E* reduce_w_partial_prefix(E *In, int n, BinaryPredicateF f){
 	E* Out = cloneA(In, n);
 	for(int i = 0; i < (int)ceil(log2(n)); i++){
 		int d = pow(2,i+1);
-		for(int j = pow(2,i)-1; j < n-1; j+=d){
+		cilk_for(int j = pow(2,i)-1; j < n-1; j+=d){
 			if (j+d/2>=n) Out[n-1]=f(Out[n-1], Out[j]);
 			else Out[j+d/2]=f(Out[j+d/2],Out[j]);
 		}
@@ -63,7 +63,8 @@ E reduce(E *In, int n, F f){
 
 int main(){
 	cout << "parallel functional interface written by Natcha Simsiri\n";
-	int A[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+	// int A[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+	int A[] = {0,1,2,3,4,5,6,7,8,9};
 	int n = sizeof(A)/sizeof(int);
 	// int *B = map(A, sizeof(A)/sizeof(int), [](int x){return x;});
 	int *B = reduce_w_partial_prefix(A, n, [](int a, int b){ return a+b; }); 
